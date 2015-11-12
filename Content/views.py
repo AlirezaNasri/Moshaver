6a39c7a110e5163d15bc4070607204db65c4ddc6
root <root@server.domain.com>
2015-11-12 13:18:38 +0330

    css bugs and views bugs

--- Content/views.py
+++ Content/views.py
@@ -4,14 +4,14 @@ from Content.models import News, Workshop, PsyContent, Interview, EduContent
 
 def get_context(news=[0, 4], psy=[0, 4], edu=[0, 4], work=[0, 1]):
 	news = News.objects.all().order_by('-pub_date')[news[0]:news[1]]
-	psyContents = PsyContent.objects.all().order_by('-pub_date')[psy[0]:psy[1]]
-	eduContents = EduContent.objects.all().order_by('-pub_date')[edu[0]:edu[1]]
+	psy_contents = PsyContent.objects.all().order_by('-pub_date')[psy[0]:psy[1]]
+	edu_contents = EduContent.objects.all().order_by('-pub_date')[edu[0]:edu[1]]
 	workshops = Workshop.objects.all().order_by('-when')[work[0]:work[1]]
 
 	return {
 		"news": news, 
-		"psyContents": psyContents, 
-		"eduContents": eduContents, 
+		"psy_contents": psy_contents, 
+		"edu_contents": edu_contents, 
 		"workshops": workshops,
 	}
 
