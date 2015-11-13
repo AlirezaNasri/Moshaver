b68a340f64735cbd5f817837d316b60fe41d7fbe
root <root@server.domain.com>
2015-11-13 10:56:51 +0330

    admin fix

--- Content/admin.py
+++ Content/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from Content.models import News, ImageContent, Workshop, PsyContent, Interview, EduContent
+from Content.models import News, ImageContent, Workshop, PsyContent, Interview, EduContent, FAQ
 
 admin.site.register(News)
 admin.site.register(Workshop)
 admin.site.register(PsyContent)
 admin.site.register(Interview)
 admin.site.register(EduContent)
-
+admin.site.register(FAQ)
 admin.site.register(ImageContent)
