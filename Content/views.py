400713f0fcf82710403cdde4b62530a9ddf34ca1
root <root@server.domain.com>
2015-12-08 02:59:13 +0330

    fixed bugs

--- Content/views.py
+++ Content/views.py
@@ -116,5 +116,5 @@ def get_gallery(req):
 	if 'page' in req.GET:
 		page = int(req.GET['page'])
 	context = get_context(page + 1 if len(ImageContent.objects.filter(caption__startswith='gallery')) > page * 10 else None, page - 1 if page != 1 else None)
-	context['gallery'] = ImageContent.objects.filter(caption='gallery')[page * 10 - 10: page * 10]
+	context['gallery'] = ImageContent.objects.filter(caption__startswith='gallery')[page * 10 - 10: page * 10]
 	return render(req, 'gallery.html', context)	
